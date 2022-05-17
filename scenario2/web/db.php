29761e453ed24959dc55dc81d2809afc568e6d97
“bernardas” <62701131+bernardas@users.noreply.github.com>
2022-05-18 00:42:42 +0300

    add scenario 2

--- scenario2/web/db.php
+++ scenario2/web/db.php
@@ -1,8 +1,4 @@
 <?php
-$dBServername = "#dBServername#";
-$dBUsername = "#dBUsername#";
-$dBPassword = "#dBPassword#";
-$dBName = "#dBName#";
 
 $con = mysqli_connect($dBServername, $dBUsername, $dBPassword, $dBName);
 if (mysqli_connect_errno())
